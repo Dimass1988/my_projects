515511c994efe6f6761b75149ac32caa454778c7
Дмитрий Маркуш <dimass1988@mail.ru>
2024-04-22 12:25:28 +0300

    Start 1.3

--- Start.py
+++ Start.py
@@ -1,12 +1,17 @@
 from random import randint
 
-player1 = input("Игрок 1, введите Ваше имя:")
-player2 = input("Игрок 2, введите Ваше имя:")
+
+def StartPlay():
+    print(f'Начинаем игру, Вам нужно угадать число от ')
+    player1 = input("Игрок 1, введите Ваше имя:")
+    player2 = input("Игрок 2, введите Ваше имя:")
+
+    print(f'{player1} введите Ваше число: ')
+
+
 rand = randint(1, 50)
 your_number = int(input("Введите число"))
 win = "Вы угадали число и выиграли!!!"
-print(f'Начинаем игру, Вам нужно угадать число от ')
-print(f'{player1} введите Ваше число: ')
 
 
 def play():
---
+++ Test/111.py
@@ -0,0 +1,13 @@
+# number = int(input())
+# num_lst = list(str(number))
+# a = list(map(int, num_lst))
+# sum_left = sum(a[0:3])
+# sum_right = sum(a[3:7])
+# if sum_right == sum_left:
+#     print("ДА")
+# else:
+#     print("НЕТ")
+#
+
+
+
