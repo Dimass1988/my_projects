655d911fa7129734bb91fc82461381643da640a0
Дмитрий Маркуш <dimass1988@mail.ru>
2024-04-30 14:13:29 +0300

    Start 1.5

--- Test/111.py
+++ Test/111.py
@@ -82,36 +82,72 @@
 #     print("28")
 
 
-month, day = map(int, input().split())
-month_previous = 0
-month_next = 0
-day_previous = 0
-day_next = 0
-if 0 < month <= 12:
-    if month == 1:
-        month_previous = 12
-    elif month == 12:
-        month_next = 1
-    month_previous, month_next = month - 1, month + 1
+# month, day = map(int, input().split())
+# month_previous = 0
+# month_next = 0
+# day_previous = 0
+# day_next = 0
+# if 0 < month <= 12:
+#     if month == 1:
+#         month_previous = 12
+#     elif month == 12:
+#         month_next = 1
+#
+#
+# print(month_previous, month_next, day)
 
-print(month_previous, month_next, day)
+
+# days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+# b, a = map(int, input().split())
+# if a == 1:
+#     pday = days[b-2]
+#     pmh = b - 1
+#     fday = 2
+#     fmh = b
+# elif a == days[b-1]:
+#     pday = a-1
+#     pmh = b
+#     fday = 1
+#     fmh = b+1
+# else:
+#     pday = a-1
+#     pmh = b
+#     fday = a+1
+#     fmh = b
+# print(f"{str(pmh).rjust(2, '0')}.{str(pday).rjust(2, '0')} "
+#       f"{str(fmh).rjust(2, '0')}.{str(fday).rjust(2, '0')}")
 
 
-days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-b, a = map(int, input().split())
-if a == 1:
-    pday = days[b-2]
-    pmh = b - 1
-    fday = 2
-    fmh = b
-elif a == days[b-1]:
-    pday = a-1
-    pmh = b
-    fday = 1
-    fmh = b+1
-else:
-    pday = a-1
-    pmh = b
-    fday = a+1
-    fmh = b
-print(f"{str(pmh).rjust(2, '0')}.{str(pday).rjust(2, '0')} {str(fmh).rjust(2, '0')}.{str(fday).rjust(2, '0')}")
+# day_num = int(input())
+# days = {0: 'воскресенье', 1: 'понедельник', 2: 'вторник', 3: 'среда',
+#         4: 'четверг', 5: 'пятница', 6: 'суббота', 7: 'воскресенье'}
+# z = day_num % 7
+# if z in days:
+#     print(days.get(z))
+# else:
+#     print('Вы ввели не верное значение!')
+
+# 4.3
+
+
+# a = float(input())
+# b = float(input())
+# d = a if a > b else b
+# print(d)
+
+
+# a = int(input())
+# msg = "кратно 3" if a % 3 == 0 else "не кратно 3"
+# print(msg)
+
+
+# word = str(input()).lower()
+# word_2 = word[::-1].lower()
+# msg = "палиндром" if word == word_2 else "не палиндром"
+# print(msg)
+
+
+num = int(input())
+print('False') if num == 0 else print('True')
+
+
